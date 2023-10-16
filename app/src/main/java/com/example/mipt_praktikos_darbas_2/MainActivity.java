14d9f7a57a76255451ce0c220f9e9e1c19762cad
DoKaUni <dovydas.kasulis@gmail.com>
2023-10-16 16:54:40 +0300

    Changed TextView text & added an EditText element

--- app/src/main/java/com/example/mipt_praktikos_darbas_2/MainActivity.java
+++ app/src/main/java/com/example/mipt_praktikos_darbas_2/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.mipt_praktikos_darbas_2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.EditText;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editableText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
