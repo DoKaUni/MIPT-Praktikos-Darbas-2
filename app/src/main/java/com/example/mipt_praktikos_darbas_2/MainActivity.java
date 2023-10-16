989aecfea16306aa9e836dd08fb7ed83d3882fe5
DoKaUni <dovydas.kasulis@gmail.com>
2023-10-16 18:19:56 +0300

    Added Button element tied with counting functionalities

--- app/src/main/java/com/example/mipt_praktikos_darbas_2/MainActivity.java
+++ app/src/main/java/com/example/mipt_praktikos_darbas_2/MainActivity.java
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.view.View;
 
 import android.os.Bundle;
 
+import Utils.CountText;
+
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textCounter;
     private EditText editableText;
     private Spinner counterOptions;
     @Override
@@ -17,9 +22,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        counterOptions = findViewById(R.id.counterOptions);
+        this.textCounter = findViewById(R.id.textCounter);
+        this.editableText = findViewById(R.id.editableText);
+        this.counterOptions = findViewById(R.id.counterOptions);
+
+        this.counterOptions = findViewById(R.id.counterOptions);
         ArrayAdapter<CharSequence> counterOptionsAdapter = ArrayAdapter.createFromResource(this, R.array.counter_options, android.R.layout.simple_spinner_item);
         counterOptionsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         counterOptions.setAdapter(counterOptionsAdapter);
+    }
+
+    public void buttonClick(View view){
+        String selectedOption = this.counterOptions.getSelectedItem().toString();
+        String[] counterOptions = getResources().getStringArray(R.array.counter_options);
+
+        String inputText = this.editableText.getText().toString();
+        if(inputText.isEmpty()){
+            System.out.println("Empty!");
+
+            return;
+        }
+
+        if(selectedOption.equals(counterOptions[0])) {
+            String wordCount = CountText.getWordCount(inputText);
+            wordCount = "Word Count: " + wordCount;
+            textCounter.setText(wordCount);
+        } else if (selectedOption.equals(counterOptions[1])){
+            String charCount = CountText.getCharCount(inputText);
+            charCount = "Character Count: " + charCount;
+            textCounter.setText(charCount);
+        }
     }
 }
