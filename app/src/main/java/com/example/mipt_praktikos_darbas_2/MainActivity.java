83e48bf0bcf8e9f037c60c07a50191415758f1ad
DoKaUni <dovydas.kasulis@gmail.com>
2023-10-16 17:15:48 +0300

    Added Spinner element with 2 selection options

--- app/src/main/java/com/example/mipt_praktikos_darbas_2/MainActivity.java
+++ app/src/main/java/com/example/mipt_praktikos_darbas_2/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.mipt_praktikos_darbas_2;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editableText;
+    private Spinner counterOptions;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        counterOptions = findViewById(R.id.counterOptions);
+        ArrayAdapter<CharSequence> counterOptionsAdapter = ArrayAdapter.createFromResource(this, R.array.counter_options, android.R.layout.simple_spinner_item);
+        counterOptionsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        counterOptions.setAdapter(counterOptionsAdapter);
     }
 }
